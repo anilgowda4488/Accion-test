39967e656eee1173a80915ca90d0f8b19316925d
ANIL HK <85819763+anilgowda4488@users.noreply.github.com>
2025-04-11 16:52:26 +0530

    Update TextManipulationProblem.sh

--- TextManipulationProblem.sh
+++ TextManipulationProblem.sh
@@ -4,7 +4,6 @@ LOG_FILE="system.log"
 
 if grep -q 'ERROR' "$LOG_FILE"; then
   awk '/ERROR/ {print toupper(substr($0, index($0, "ERROR")))}' "$LOG_FILE"
-
   echo ""
   echo "Extracting and transforming ERROR messages:"
   grep 'ERROR' "$LOG_FILE" | sed 's/.*ERROR: /ISSUE: /' | tr '[:lower:]' '[:upper:]'
